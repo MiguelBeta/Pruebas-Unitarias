db89c8487663029da945a62d6589724c623bdaaf
Miguel <workingbeta01@gmail.com>
2024-06-11 19:33:45 -0500

    Simular confirmaciones de usuario

--- src/app/intermedio/espias/medicos.component.spec.ts
+++ src/app/intermedio/espias/medicos.component.spec.ts
@@ -3,7 +3,7 @@ import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 // import { Observable } from 'rxjs';
-import { Observable, from, EMPTY } from 'rxjs';
+import { Observable, from, EMPTY, throwError } from 'rxjs';
 
 
 describe('MedicosComponent', () => {
@@ -72,4 +72,42 @@ describe('MedicosComponent', () => {
   });
 
 
+  it( 'si falla la adición, la propiedad mensajeError, debe ser igual al error del servicio', () => {
+
+    const miError = 'No se puedo agregar el medico';
+
+    spyOn( servicio, 'agregarMedico' ).and
+            .returnValue( throwError( miError ) );
+
+    componente.agregarMedico();
+
+    expect( componente.mensajeError ).toBe( miError );
+
+  });
+
+  it( 'Debe de llamar al servidor para borrar un médico', () => {
+
+    spyOn( window, 'confirm' ).and.returnValue( true );
+
+    const espia = spyOn( servicio, 'borrarMedico' ).and
+                          .returnValue( EMPTY );
+
+    componente.borrarMedico( '1' );
+
+    expect( espia ).toHaveBeenCalledWith( '1' );
+
+  });
+
+  it( 'NO debe de llamar al servidor para borrar un médico', () => {
+
+    spyOn( window, 'confirm' ).and.returnValue( false );
+
+    const espia = spyOn( servicio, 'borrarMedico' ).and
+                          .returnValue( EMPTY );
+
+    componente.borrarMedico( '1' );
+
+    expect( espia ).not.toHaveBeenCalledWith( '1' );
+
+  });
 });
